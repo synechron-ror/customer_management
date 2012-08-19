8424629c06de59ce4c20e9c1a4387a3c15338610
SP <saurabhp@synechron.com>
2012-08-19 12:45:39 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CustomerManagement::Application.config.secret_token = '11c202ea3d9533c55999e2b9e1abff32a3f0a8ba060909084d98e0283a94ce490ea41b8a3d71c27a5aaf21816ccc174fb6789984ffca6d4662b8fd2922762453'
