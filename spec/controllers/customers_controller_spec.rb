bb521c7b2a86332b2322924fa70cb694b7ade8c3
SP <saurabhp@synechron.com>
2012-08-19 17:42:34 -0400

    successfully started with controller testst

--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -2,4 +2,13 @@ require 'spec_helper'
 
 describe CustomersController do
 
+  describe 'Actions' do
+
+    it "should get index successfully" do
+       get :index
+       response.should be_success
+    end
+
+    
+  end
 end
